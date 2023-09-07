1ff15e03a7d4fcb209895ab22eb22d30ec3df495
cldunlap73 <christydunlap26@gmail.com>
2023-09-07 10:10:40 -0500

    first commit

---
+++ app/src/main/java/com/example/classificationmodel/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.classificationmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+
+import androidx.annotation.Nullable;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.ThumbnailUtils;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.content.Context;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.FileUtil;
+import java.io.IOException;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button camera, gallery;
+    ImageView imageView;
+    TextView result;
+    int imageSize=32;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        camera=findViewById(R.id.button);
+        gallery=findViewById(R.id.button2);
+
+        result=findViewById(R.id.result);
+        imageView=findViewById(R.id.imageView);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if(checkSelfPermission(Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED){
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 3);
+                }else{
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+                }
+            }
+        });
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(cameraIntent, 3);
+
+            }
+        });
+    }
+
+    public void classify(Bitmap image){
+        TensorBuffer outputFeature0=null;
+        try {
+
+
+            Interpreter.Options options=new Interpreter.Options();
+            Interpreter interpreter=new Interpreter(FileUtil.loadMappedFile(getApplicationContext(), "model1.tflite"));
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 32, 32, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer=ByteBuffer.allocateDirect(4*imageSize*imageSize*3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            //iterate over each pixel and extract R, G, and B values. Add those values individually to the byte buffer.
+            for(int i = 0; i < imageSize; i ++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++]; // RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 1));
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            //Interpreter.Outputs outputs = interpreter.process(inputFeature0);
+
+            //TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+
+            interpreter.run(inputFeature0.getBuffer(), outputFeature0.getBuffer());
+            float[] confidences=outputFeature0.getFloatArray();
+            int maxPos=0;
+            float maxConfidence=0;
+            for (int i=0; i <confidences.length; i++){
+                if (confidences[i]>maxConfidence){
+                    maxConfidence=confidences[i];
+                    maxPos=i;
+                }
+            }
+            String[] classes={"Bird","Cat","Fish"};
+            result.setText(classes[maxPos]);
+            // Releases model resources if no longer used.
+            interpreter.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(resultCode==RESULT_OK){
+            if(requestCode==3){
+                Bitmap image=(Bitmap) data.getExtras().get("data");
+                int dimension = Math.min(image.getWidth(), image.getHeight());
+                image= ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+                imageView.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classify(image);
+
+            }else{
+                Uri dat =data.getData();
+                Bitmap image=null;
+                try {
+                    image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), dat);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+                imageView.setImageBitmap(image);
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classify(image);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
